2c5c264e234f7aa347d2303cb625d66d119bb06f
Madhurjya Gogoi <madhurjyagogoi@Madhurjyas-MacBook-Air.local>
2022-02-18 13:46:30 +0530

    new version

--- src/container/Header.js
+++ src/container/Header.js
@@ -4,7 +4,7 @@ import { Jumbotron } from 'react-bootstrap';
 function Header() {
     return (
         <Jumbotron>
-            <h1>Welcome to MyShop</h1>
+            <h1>Welcome to MyShop. We are welcoming you to our shop </h1>
         </Jumbotron>
 
     )
